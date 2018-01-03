e4fc8222d46d230eb38dc727918d364d0a3bd7b6
User <niteshgarg2107@gmail.com>
2018-01-03 20:06:12 +0530

    Resolve Assign Value issue

--- ExtensionObject/ViewController.swift
+++ ExtensionObject/ViewController.swift
@@ -7,15 +7,22 @@
 //
 
 import UIKit
+struct AssociatedKeys {
+    static var toggleState: UInt8 = 0
+}
+enum TogleState {
+    case on
+    case off
+}
 
 class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         let btn = UIButton()
-        print(state(State: btn.State))
+//        state(State: btn.State)
         btn.State = .on
-        print(state(State: btn.State))
+        state(State: btn.State)
     }
     func state(State: TogleState) -> Void {
         switch State {
@@ -30,26 +37,30 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 }
-struct AssociatedKeys {
-    static var toggleState: UInt8 = 0
-}
-enum TogleState {
-    case on
-    case off
-}
 
 extension UIButton {
     var State: TogleState {
         get {
-            guard objc_getAssociatedObject(self, &AssociatedKeys.toggleState) != nil else {
+            guard let value = objc_getAssociatedObject(self, &AssociatedKeys.toggleState) as? TogleState else {
                 return .off
             }
-            return objc_getAssociatedObject(self, &AssociatedKeys.toggleState) as! TogleState
+            return value
         }
-        set(newvalue) {
-            objc_setAssociatedObject(self, &AssociatedKeys.toggleState, newvalue, .OBJC_ASSOCIATION_ASSIGN)
+        set(newValue) {
+            objc_setAssociatedObject(self, &AssociatedKeys.toggleState, newValue, objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN_NONATOMIC)
         }
     }
 }
 
-
+/*
+OBJC_ASSOCIATION_ASSIGN = 0,           /**< Specifies a weak reference to the associated object. */
+OBJC_ASSOCIATION_RETAIN_NONATOMIC = 1, /**< Specifies a strong reference to the associated object.
+ *   The association is not made atomically. */
+OBJC_ASSOCIATION_COPY_NONATOMIC = 3,   /**< Specifies that the associated object is copied.
+ *   The association is not made atomically. */
+OBJC_ASSOCIATION_RETAIN = 01401,       /**< Specifies a strong reference to the associated object.
+ *   The association is made atomically. */
+OBJC_ASSOCIATION_COPY = 01403          /**< Specifies that the associated object is copied.
+ *   The association is made atomically. */
+};
+*/
